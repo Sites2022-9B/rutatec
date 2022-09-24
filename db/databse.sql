1563ceed6d779952b869da9fa893fd425f37412e
Henry Lopez Vazquez <lopezvazquezhenry@gmail.com>
2022-09-24 14:33:32 -0500

    Nuevo script para la BD Signed-off-by: Henry Lopez Vazquez <lopezvazquezhenry@gmail.com>

---
+++ db/databse.sql
@@ -0,0 +1,77 @@
+CREATE TABLE IF NOT EXISTS pages (
+	id SERIAL NOT NULL,
+	nombre VARCHAR(50),
+	url VARCHAR(50),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+	id SERIAL NOT NULL,
+	nombre VARCHAR (50),
+	correo VARCHAR (50),
+	contra VARCHAR (30),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS rutas (
+	id SERIAL NOT NULL,
+	nombre VARCHAR (30),
+	descripcion VARCHAR (100),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS rutguardadas (
+	id SERIAL NOT NULL,
+	user_id INT NOT NULL,
+	rutas_id INT NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT rutguardadas_user_id_fkey 
+	FOREIGN KEY (user_id) REFERENCES usuario (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT rutguardadas_rutas_id_fkey 
+	FOREIGN KEY (rutas_id)REFERENCES rutas (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS puntrutas (
+	id SERIAL NOT NULL,
+	rutas_id INT NOT NULL,
+	longitud VARCHAR (30),
+	latitud VARCHAR (30),
+	PRIMARY KEY (id),
+	CONSTRAINT puntrutas_rutas_id_fkey 
+	FOREIGN KEY (rutas_id)REFERENCES rutas (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS historial (
+	id SERIAL NOT NULL,
+	user_id INT NOT NULL,
+	rutas_id INT NOT NULL,
+	fecha DATE,
+	PRIMARY KEY (id),
+	CONSTRAINT historial_rutas_id_fkey 
+	FOREIGN KEY (rutas_id)REFERENCES rutas (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT rutguardadas_user_id_fkey 
+	FOREIGN KEY (user_id) REFERENCES usuario (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS municipio (
+	id SERIAL NOT NULL,
+	nombre VARCHAR (30),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS terminales (
+	id SERIAL NOT NULL,
+	munic_id INT NOT NULL,
+	nombre VARCHAR(50),
+	longitud VARCHAR (100),
+	latitud VARCHAR (100),
+	PRIMARY KEY(id),
+	CONSTRAINT terminales_munic_id_fkey 
+	FOREIGN KEY (munic_id) REFERENCES municipio (id)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
