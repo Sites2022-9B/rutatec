bfb160a42980f32bbdbde523566e627a20dd66bd
Henry Lopez Vazquez <lopezvazquezhenry@gmail.com>
2022-11-02 12:10:22 -0600

    Se agregaron las funcionalidades del mapa con las coordenadas, se hicieron insert de las coordenadas reales de una ruta, se agregó un alert para el checkbox de aviso de privacidad.
    Signed-off-by: Henry Lopez Vazquez <lopezvazquezhenry@gmail.com>

--- modulos/personal/r_index.py
+++ modulos/personal/r_index.py
@@ -13,3 +13,11 @@ async def getrutas(session: Tuple[SessionData, str] = Depends(test_session), db:
     metadata,rows = database.execSql(sql, sqlParams, False, True)
     print('rutas',rows)
     return{"metadata": metadata, "data": rows}
+
+@router.get('/api/puntrutas/{id_ruta}')
+async def getrutas(id_ruta: str ,session: Tuple[SessionData, str] = Depends(test_session), db: Session = Depends(database.get_db)):
+    sql = 'SELECT latitud, longitud FROM puntrutas Where rutas_id =' + id_ruta
+    sqlParams = {}
+    rows = database.execSql(sql, sqlParams, False, True)
+    print('rutas',rows)
+    return{"data": rows}
